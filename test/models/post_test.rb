df013336018117639f8432eea70ea67a76d42f87
dhiraj-cis <dhiraj.y@cisinlabs.com>
2024-09-03 18:47:01 +0530

    changes in test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -6,12 +6,12 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "The Post should not be save without title" do
+  test 'The Post should not be save without title' do
     post = Post.new
-    assert_not post.save, "Saved The Post Without The Title"
+    assert_not post.save, 'Saved The Post Without The Title'
   end
-  test "should sabe post with title" do 
-    post = Post.new(title: "test")
-    assert post.save, "Idk words goes here"
+  test 'should sabe post with title' do
+    post = Post.new(title: 'test')
+    assert post.save, 'Idk words goes here'
   end
 end
